850d382e1624b6c407d1a2137c7d85efb6063f98
tanapong2114 <uncubphom@gmail.com>
2021-03-20 15:56:37 +0700

    Start App

--- app/src/main/java/com/example/permission/MainActivity.java
+++ app/src/main/java/com/example/permission/MainActivity.java
@@ -1,20 +1,39 @@
 package com.example.permission;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     static final int RESULT_LOAD_IMAGE=321 ;
     static final int REQUEST_IMAGE_PERMISSION= 111;
+    private StorageReference mStorageRef;
+    private ImageView imageView;
+    private Uri selectedImage;
+    private Button btn_select , btn_upload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +41,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        btn_select = findViewById(R.id.btn_select);
+        btn_upload = findViewById(R.id.btn_upload);
+        imageView = findViewById(R.id.imageView);
+
+        btn_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, RESULT_LOAD_IMAGE);
+            }
+        });
+
+        btn_select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mStorageRef.child("Image/"+ UUID.randomUUID()+".jpg").putFile(selectedImage)
+                        .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                Toast.makeText(MainActivity.this,"success",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
         //เรียกใช้
         checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, REQUEST_IMAGE_PERMISSION);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data){
+
+            selectedImage = data.getData();
+            imageView.setImageURI(selectedImage);
+
+        }
     }
 
     //Function to check
